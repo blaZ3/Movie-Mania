685ab369ab851118af7afa0a47873be90b158a4d
Vivekanandan <vivekanandan@hammerhead.io>
2019-01-22 06:26:27 +0530

    project init

---
+++ app/src/main/java/com/example/moviemania/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.moviemania
+
+import org.koin.dsl.module.module
+
+class AppModule {
+
+    companion object {
+
+        private val appModule = module {
+
+        }
+
+        val appModules = listOf(
+            appModule
+        )
+
+    }
+
+}
