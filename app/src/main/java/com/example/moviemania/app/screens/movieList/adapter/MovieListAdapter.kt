d91161115cd25ba9764632aa94af3bbc9d07735c
Vivekanandan <vivekanandan@hammerhead.io>
2019-01-27 20:06:53 +0530

    added lottie

--- app/src/main/java/com/example/moviemania/app/screens/movieList/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/example/moviemania/app/screens/movieList/adapter/MovieListAdapter.kt
@@ -31,7 +31,8 @@ class MovieListAdapter(
         fun onBind(item: MovieListDataItem) {
             view.recyclerFavoriteMovies.layoutManager = LinearLayoutManager(
                 context, LinearLayoutManager.HORIZONTAL,
-                false)
+                false
+            )
             val favoriteListAdapter = FavoriteListAdapter(
                 context = context,
                 items = item.item as List<Movie>,
@@ -51,14 +52,6 @@ class MovieListAdapter(
                 Picasso.get().load(item.poster).into(view.imgMovieCardPoster)
             }
 
-            view.imgMovieCardDoFavoriteDone.visibility = View.GONE
-            view.imgMovieCardDoFavorite.visibility = View.GONE
-            if (item.favorited) {
-                view.imgMovieCardDoFavoriteDone.visibility = View.VISIBLE
-            } else {
-                view.imgMovieCardDoFavorite.visibility = View.VISIBLE
-            }
-
             view.imgMovieCardPoster.setOnClickListener {
                 movieListAdapterInterface?.onMovieSelectedAtPosition(adapterPosition)
             }
@@ -66,10 +59,35 @@ class MovieListAdapter(
                 movieListAdapterInterface?.onMovieSelectedAtPosition(adapterPosition)
             }
 
-            view.imgMovieCardDoFavorite.setOnClickListener {
-                movieListAdapterInterface?.onMovieFavoritedAtPosition(adapterPosition)
+            view.imgMovieCardDoFavoriteDone.visibility = View.GONE
+            view.imgMovieCardDoFavorite.visibility = View.GONE
+//            if (item.favorited) {
+//                view.imgMovieCardDoFavoriteDone.visibility = View.VISIBLE
+//            } else {
+//                view.imgMovieCardDoFavorite.visibility = View.VISIBLE
+//            }
+//
+//            view.imgMovieCardDoFavorite.setOnClickListener {
+//                movieListAdapterInterface?.onMovieFavoritedAtPosition(adapterPosition)
+//            }
+//            view.imgMovieCardDoFavoriteDone.setOnClickListener {
+//                movieListAdapterInterface?.onMovieFavoritedAtPosition(adapterPosition)
+//            }
+
+
+
+            view.lottieFavorite.setMinAndMaxProgress(0.0f, 1.0f)
+            if (item.favorited) {
+                view.lottieFavorite.setMinAndMaxFrame(0, 240)
+            } else {
+                view.lottieFavorite.setMinAndMaxFrame(0, 60)
             }
-            view.imgMovieCardDoFavoriteDone.setOnClickListener {
+            view.lottieFavorite.apply {
+                speed = 1.5f
+                playAnimation()
+            }
+            view.lottieFavorite.resumeAnimation()
+            view.lottieFavorite.setOnClickListener {
                 movieListAdapterInterface?.onMovieFavoritedAtPosition(adapterPosition)
             }
         }
@@ -78,8 +96,10 @@ class MovieListAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {
         if (viewType == TYPE_FAVORITES) {
-            val view = LayoutInflater.from(context).inflate(R.layout.layout_item_favorites, parent,
-                false)
+            val view = LayoutInflater.from(context).inflate(
+                R.layout.layout_item_favorites, parent,
+                false
+            )
 
             return MovieFavoritesViewHolder(
                 view,
@@ -134,7 +154,7 @@ class MovieListAdapter(
         fun onMovieSelectedAtPosition(position: Int)
         fun onMovieSelected(item: SearchResultItem)
 
-//        fun onMovieFavorited(item: SearchResultItem)
+        //        fun onMovieFavorited(item: SearchResultItem)
         fun onMovieFavoritedAtPosition(position: Int)
     }
 
